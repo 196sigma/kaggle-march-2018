631a85e80c3d2642f0e8ab2b6b80acdef09f9df6
Reginald Edwards <reginald.edwards@gmail.com>
2018-03-02 10:38:23 -0600

    added template.R

---
+++ code/template.R
@@ -0,0 +1,9 @@
+## Reginald Edwards
+## CREATED: 2 March 2018
+## MODIFIED:
+## DESCRIPTION: Compute the probability regular season win percentages and use
+## ase probability of tournament win
+###############################################################################
+
+source('code/prep-data.R')
+source('code/add-features.R')
